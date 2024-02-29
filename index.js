ca3f6dfbddd77e0d83e3b91a1370faf3aa6ef2b1
Johannes Lumbantoruan <johannestrikardo@gmail.com>
2024-02-29 11:52:51 +0700

    Finish the challenge

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+require('dotenv').config();
+
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const multer = require('multer');
 
 var app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +24,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const img = req.file;
 
-
+  res.json({
+    name: img.originalname,
+    type: img.mimetype,
+    size: img.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
